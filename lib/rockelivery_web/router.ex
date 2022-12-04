d4b76c7fe0cdd6d6601822bc8a62d0d12f6a1e7c
WilliamJagerDEV <williamjager.dev@gmail.com>
2022-12-04 18:44:42 -0300

    API created - save user

--- lib/rockelivery_web/router.ex
+++ lib/rockelivery_web/router.ex
@@ -9,7 +9,8 @@ defmodule RockeliveryWeb.Router do
     pipe_through :api
 
     get "/", WelcomeController, :index
-  end
+    resources "/users", UsersController, except: [:new, :edit]
+    end
 
   # Enables LiveDashboard only for development
   #
