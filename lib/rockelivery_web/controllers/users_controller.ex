052b5dad722e07b0e9e6526e75afe7d0c1c95d9b
WilliamJagerDEV <williamjager.dev@gmail.com>
2022-12-05 10:59:00 -0300

    fallback render bad_request

--- lib/rockelivery_web/controllers/users_controller.ex
+++ lib/rockelivery_web/controllers/users_controller.ex
@@ -2,6 +2,9 @@ defmodule RockeliveryWeb.UsersController do
   use RockeliveryWeb, :controller
 
   alias Rockelivery.User
+  alias RockeliveryWeb.FallbackController
+
+  action_fallback FallbackController
 
   def create(conn, params) do
     with {:ok, %User{} = user} <- Rockelivery.create_user(params) do
@@ -11,4 +14,3 @@ defmodule RockeliveryWeb.UsersController do
     end
   end
 end
-
