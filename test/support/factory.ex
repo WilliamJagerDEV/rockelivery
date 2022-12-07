18a3807ae12701105e5c8c3b2c108062f49b8a96
WilliamJagerDEV <williamjager.dev@gmail.com>
2022-12-06 22:26:34 -0300

    create_test

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -3,13 +3,13 @@ defmodule Rockelivery.Factory do
 
   def user_params_factory do
     %{
-        address: "Rua Guanabara, n56",
-        age: 37,
-        cep: "29164815",
-        cpf: "11839027789",
-        email: "williamjager.dev@gmail.com",
-        password: "123456",
-        name: "William"
-      }
+      address: "Rua Guanabara, n56",
+      age: 37,
+      cep: "29164815",
+      cpf: "11839027789",
+      email: "williamjager.dev@gmail.com",
+      password: "123456",
+      name: "William"
+    }
   end
 end
